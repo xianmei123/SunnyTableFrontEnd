75da81a2cd4f9a28b5cec8d98f5d44ad9c894517
18373439 <18373439@buaa.edu.cn>
2021-04-28 17:24:50 +0800

    解决横坐标无法进行拖拽的bug，并成功修复。现在能够维持折线图各种数据的持续拖拽#11

--- pages/draw/draw.js
+++ pages/draw/draw.js
@@ -3,9 +3,28 @@
 import * as echarts from '../../ec-canvas/echarts';
 
 class LineGraph {
+    // 单个折线图时，需要考虑横纵坐标值的类型，如果横坐标或纵坐标中有一个是字符串类型，则设置对应的坐标轴中的
+    // xAxis和yAxis的type类型为category，并设置boundaryGap为false（表示坐标轴中不存在区间值。），然后在初始画图时对对应坐标轴
+    // 设置xMap 或 yMap 来映射点的下标和其对应的字符串，然后在对点进行拖动时重新绘图时需要将对应坐标数值转换为字符串，悲伤的事实证明这种方法画出来的图
+    // 会出现拖动一段时间之后，无法再进行拖动的bug。
+
+    // 重构的方法：在有一方为字符串时，为此方确定对应的data，然后在series中仅仅设置另外一方的data，并在转换时仅仅转换非字符串一方的坐标
+    // 这样就会简化掉上一种方法中的更新操作时的对坐标的替换，理论上保证了不会出现bug！！！
+    // 
+
+
+    // 如果横纵坐标值的类型均为字符串，则xAxis和yAxis都设置为category，并设置图形不可拖拽，除此情况之外均可进行拖拽
+    // 
+
+    // 如果为数值类型，则将type设置为value，根据情况设置拖拽范围即xAxis中的min和max属性。
+
+
+    // 多个折线图展示时，横纵坐标类型应该相同。不同时抛出异常
+
+    // 多个折线图需要在option中添加多个{},即可，每一个数据需要自行指派。
     lineChart;
     inputList = []; // 
-    pointToName = new Map(); // <index, name>
+    pointToName = []; // <index, name>
     nameToIndex = {};
 
     xMap = new Map(); // <data.index, data.index.x> // data中每一个数组在data中下标-> 该下标对应的两个元素的x坐标
@@ -32,42 +51,23 @@ class LineGraph {
     constructor() {}
 
     setLineModel() {
-
     }
 
     setInputList(name, data) {
-        console.log(name, data);
         this.nameToIndex[name] = {
             "minIndex": this.inputList.length,
             "maxIndex": this.inputList.length + data.length
         };
         for (var i = 0; i < data.length; i++) {
-            this.pointToName.set(this.inputList.length + i, name);
+            this.pointToName.push(name);
         }
-        
         for (var i in data) {
             this.inputList.push(data[i]);
         }
     }
-    setXMap() {}
 }
 
 var line = new LineGraph();
-
-// 单个折线图时，需要考虑横纵坐标值的类型，如果横坐标或纵坐标中有一个是字符串类型，则设置对应的坐标轴中的
-// xAxis和yAxis的type类型为category，并设置boundaryGap为false（表示坐标轴中不存在区间值。），然后在初始画图时对对应坐标轴
-// 设置xMap 或 yMap 来映射点的下标和其对应的字符串，然后在对点进行拖动时重新绘图时需要将对应坐标数值转换为字符串。
-
-
-// 如果横纵坐标值的类型均为字符串，则xAxis和yAxis都设置为category，并设置图形不可拖拽，除此情况之外均可进行拖拽
-
-// 如果为数值类型，则将type设置为value，根据情况设置拖拽范围即xAxis中的min和max属性。
-
-
-// 多个折线图展示时，横纵坐标类型应该相同。不同时抛出异常
-
-// 多个折线图需要在option中添加多个{},即可，每一个数据需要自行指派。
-
 
 Page({
     data: {
@@ -125,11 +125,9 @@ function initLineChart(canvas, width, height, dpr) {
     canvas.setChart(lineChart);
 
     var option = setOption();
-    lineChart.setOption(option);
+    lineChart.setOption(option); // 
     lineChart.setOption({
         graphic: echarts.util.map(line.inputList, function (dataItem, dataIndex) {
-            console.log(dataIndex);
-            var position = lineChart.convertToPixel('grid', dataItem);
             if (line.xType === 'string') {
                 line.xMap.set(dataIndex, dataItem[0]);
             }
@@ -138,7 +136,7 @@ function initLineChart(canvas, width, height, dpr) {
             }
             return {
                 type: 'circle',
-                position: position,
+                position: line.lineChart.convertToPixel('grid', dataItem),
                 shape: {
                     r: line.lineModelPro.radius / 2
                 },
@@ -146,21 +144,19 @@ function initLineChart(canvas, width, height, dpr) {
                 draggable: line.draggable,
                 ondrag: echarts.util.curry(onPointDragging, dataIndex),
                 z: 100,
-                //onmousemove: echarts.util.curry(showTooltip, dataIndex),
-                //onmouseout: echarts.util.curry(hideTooltip, dataIndex),
+                onmousemove: echarts.util.curry(showTooltip, dataIndex),
+                onmouseout: echarts.util.curry(hideTooltip, dataIndex),
             };
         })
     });
-
     console.log("success");
     return lineChart;
 }
 
-
 function setOption() {
-    var series = [];
+    var series=[];
     for (var i = 0; i < line.inputList.length;) {
-        var name = line.pointToName.get(i);
+        var name = line.pointToName[i];
         var tempJson = {
             name: name,
             type: "line",
@@ -168,23 +164,22 @@ function setOption() {
             symbolSize: line.lineModelPro.radius,
             data: line.inputList.slice(line.nameToIndex[name].minIndex, line.nameToIndex[name].maxIndex)
         };
-        i = line.nameToIndex[name].maxIndex
+        i = line.nameToIndex[name].maxIndex;
         series.push(tempJson);
     }
     var option = {
-        // tooltip: {
-        //     // 表示不使用默认的『显示』『隐藏』触发规则。
-        //     triggerOn: 'none',
-        //     formatter: function (params) {
-        //         return 'X: ' + params.data[0].toFixed(2) + '\nY: ' + params.data[1].toFixed(2);
-        //     }
-        // },
+        tooltip: {
+            triggerOn: 'none',
+            formatter: function (params) {
+                return 'X: ' + params.data[0] + '\nY: ' + params.data[1].toFixed(2);
+            }
+        },
         xAxis: {
             type: line.xType === "string" ? 'category' : 'value',
             boundaryGap: !(line.xType === "string"),
             axisLine: {
                 onZero: false
-            },
+            }
         },
         yAxis: {
             min: -30,
@@ -194,7 +189,6 @@ function setOption() {
             axisLine: {
                 onZero: false
             },
-
         },
         series: series
     };
@@ -205,23 +199,20 @@ function onPointDragging(dataIndex) {
     // 这里的 data 就是本文最初的代码块中声明的 data，在这里会被更新。
     // 这里的 this 就是被拖拽的圆点。this.position 就是圆点当前的位置。
     line.inputList[dataIndex] = line.lineChart.convertFromPixel('grid', this.position);
-    if (line.xType === "string" ) {
+    if (line.xType === "string") {
         line.inputList[dataIndex][0] = line.xMap.get(dataIndex);
     }
-    if (line.yType === "string") {
-        line.inputList[dataIndex][1] = line.yMap.get(dataIndex);
-    }
-    var name = line.pointToName.get(dataIndex);
+    var name = line.pointToName[dataIndex];
+    this.position = [line.lineChart.convertToPixel({xAxisIndex: 0}, line.xMap.get(dataIndex)),this.position[1]];
     setTimeout(function () {
         line.lineChart.setOption({
             series: [{
                 name: name,
-                data: line.inputList.slice(line.nameToIndex[name].minIndex, line.nameToIndex[name].maxIndex)
+                data: line.inputList
             }]
         });
     }, 0);
 }
-
 function showTooltip(dataIndex) {
     line.lineChart.dispatchAction({
         type: 'showTip',
