f6dca9930cfe20c3c08d33a7bb44c28cab049ab9
xian mei <1586149310@qq.com>
2021-05-31 23:19:00 +0800

    #32 design bill page

--- pages/bill/bill.js
+++ pages/bill/bill.js
@@ -1,66 +1,71 @@
-// pages/bill/bill.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        show: false,
+        sore: false,
+        startDate: new Date().toLocaleDateString(),
+        endDate: new Date().toLocaleDateString(),
+        currentDate: null,
+        minDate: new Date(2021, 3, 1).getTime(),
+        formatter(type, value) {
+            if (type === 'year') {
+                return `${value}年`;
+            } 
+            if (type === 'month') {
+                return `${value}月`;
+            }
+            //console.log(value)
+            return value;
+  
+        },
+    },
+  
+    onConfirm(event) {
+        var date = new Date(event.detail);
+        console.log(date.toLocaleDateString())
+        console.log(this.data.sore)
+        if (this.data.sore) {
+            console.log("true")
+            this.setData({
+                currentDate: event.detail,
+                endDate: date.toLocaleDateString(),
+                show : false,
+            });
+        }
+        else {
+            console.log("false")
+            this.setData({
+                currentDate: event.detail,
+                startDate: date.toLocaleDateString(),
+                show : false,
+            });
+        }
+        
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    showPopup1() {
+        this.setData({ show: true, sore : false });
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+    showPopup2() {
+        this.setData({ show: true, sore : true });
+    },
+    
+    onClose() {
+        this.setData({ show: false });
+    },
+    
+    onDisplay() {
+        this.setData({ show: true });
+    },
+    onClose() {
+        this.setData({ show: false });
+    },
+    
+    onInput(event) {
+        this.setData({
+            currentDate: event.detail,
+        });
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
-})
+    
+});
